e4301e2ccc28f0eafdf6c5922ead1f292ec02d73
Matthew <matthew@spaceshipaudio.co.uk>
2017-05-13 21:57:19 +0100

    WIP

--- RegisterView.py
+++ RegisterView.py
@@ -1,9 +1,91 @@
-from xml.etree.ElementTree import ElementTree
+import xml.etree.ElementTree as ElementTree
+import copy
+#from xml.etree.ElementTree import ElementTree
 from os import path
 
+# This is just here for reference...
+#
+#  def print_reg(self, name, val):
+#    print "%s (*0x%08X) = 0x%08X\n" % (name, self.get_reg_address(name), val)
+#    reg = self.get_reg_element(name)
+#    for field in reg.getchildren():
+#      bit_len = int(field.attrib['bitlength'])
+#      bit_offset = int(field.attrib['bitoffset'])
+#      bit_name = field.attrib['name']
+#      description = field.attrib.get('description', 'no description')
+#      print "%s\t0x%X\t\t%s" % (bit_name, self.extract_bits(val, bit_len, bit_offset), description)
+#
+# set('name', value) sets an attribute
+# SubElement(parent, tag, attrrib={}, **extra) adds sub element to parent
+
 class RegisterView:
+  
+  def adapt_elements_to_peripheral(self, elements, new_peripheral_name, new_base_address)
+    new_elements = copy.deepcopy(element) 
+    for r in new_elements.findall('.//register'):
+      register_name = r.find('name').text
+      fullname = new_peripheral_name + '_' + register_name
+      r.set('fullname', fullname) 
+      print "d   %s" %(fullname)
+      # Calculate absolute address
+      offset = r.find('addressOffset').text 
+      r.set('address', hex(int(new_base_address,16) + int(offset,16)))
+   
+    return new_elements
+ 
+  def load_svd_definitions(self, svd_file):
+    self.tree = ElementTree.ElementTree()
+    self.tree.parse(path.expanduser(svd_file))
+
+    # Parse the svd defs into eclipse format 
+    peripherals = self.tree.findall('.//peripheral')
+
+    for peripheral in peripherals:
+      peripheral_name = peripheral.find('name').text
+      base_address = peripheral.find('baseAddress').text
+      print "peripheral name = %s" %(peripheral_name)
+      try:
+        derived_from = peripheral.attrib['derivedFrom']
+        elements = filter(lambda x: x.attrib['fullname'] == derived_from, self.tree.findall('.//peripheral'))
+        if len(elements) <= 0:
+          error_msg = "ERROR could not find peripheral %s info derived from %s" %(peripheral_name, derived_from)
+          raise Exception(error_msg)
+
+        # Create a duplicate in this peripheral
+        print "derived!!"
+        new_elements = adapt_elements_to_peripherals(self, elements, peripheral_name, base_address)
+        peripheral.insert(new_elements)
+        
+        # No need to do anything else for this peripheral
+        continue
+
+      except KeyError:
+        # Not derived from anything... proceed
+        pass
+
+      for r in peripheral.findall('.//register'):
+        register_name = r.find('name').text
+        fullname = peripheral_name + '_' + register_name
+        r.set('fullname', fullname) 
+        print "   %s" %(fullname)
+        
+        # Calculate absolute address
+        offset = r.find('addressOffset').text 
+        r.set('address', hex(int(base_address,16) + int(offset,16)))
+      
+        for f in r.findall('.//field'):
+          new_attribs = {'bitlength':f.find('bitWidth').text,
+                         'bitoffset':f.find('bitOffset').text,
+                         'name':f.find('name').text,
+                         'description':f.find('description').text};
+
+          # Shove it back into the tree 
+          ElementTree.SubElement(r, 'field', new_attribs);
+
+    self.reg_defs = self.tree.getiterator('register')
+
   def load_definitions(self, defs_file):
-    self.tree = ElementTree()
+    self.tree = ElementTree.ElementTree()
     self.tree.parse(path.expanduser(defs_file))
     reggroups = self.tree.findall(".//registergroup")
     for rg in reggroups:
--- gdb-regview.py
+++ gdb-regview.py
@@ -27,6 +27,17 @@ class RegviewLoadCommand(gdb.Command):
   def invoke(self, arg, from_tty):
     rv.load_definitions(arg)
 
+class RegviewLoadSvdCommand(gdb.Command):
+  "Load register definitions from SVD XML file."
+
+  def __init__(self):
+    super (RegviewLoadSvdCommand, self).__init__ ("regview loadsvd",
+      gdb.COMMAND_SUPPORT,
+      gdb.COMPLETE_FILENAME)
+
+  def invoke(self, arg, from_tty):
+    rv.load_svd_definitions(arg)
+
 class RegviewShowCommand(gdb.Command):
   "Show the value of a register."
 
@@ -49,8 +60,9 @@ class RegviewShowCommand(gdb.Command):
 
 RegviewPrefixCommand()
 RegviewLoadCommand()
+RegviewLoadSvdCommand()
 RegviewShowCommand()
 
 if __name__ == '__main__':
-  print 'Loaded', __file__
+  print 'Loaded!', __file__
   print 'Type "regview <tab>" to see available commands.'
